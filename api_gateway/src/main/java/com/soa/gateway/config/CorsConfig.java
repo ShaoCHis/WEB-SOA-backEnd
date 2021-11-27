fe2725b5697f9d7d1a96258d0c99ffbf44fc0513
Bobo <572025842@qq.com>
2021-11-27 21:43:51 +0800

    1

---
+++ api_gateway/src/main/java/com/soa/gateway/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.soa.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/*
+ * @author ShenBo
+ * @description 处理跨域
+ * @date 2021/11/27 21:36
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
+
