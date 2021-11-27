441dc07bc1119c933e70be5a82aaa0324af0b52c
Bobo <572025842@qq.com>
2021-11-27 20:35:46 +0800

    修改部分dependency

--- api_gateway/src/main/java/com/soa/gateway/ApiGatewayApplication.java
+++ api_gateway/src/main/java/com/soa/gateway/ApiGatewayApplication.java
@@ -3,10 +3,8 @@ package com.soa.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@EnableDiscoveryClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
