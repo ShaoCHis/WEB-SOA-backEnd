8fd6e9da522517fd65145fca3e96e93ecb5b5f16
Bobo <572025842@qq.com>
2021-11-27 21:25:51 +0800

    gateway模块不能引入commonutils模块的依赖，不然会无法启动，因为有swagger的依赖

--- api_gateway/src/main/java/com/soa/gateway/ApiGatewayApplication.java
+++ api_gateway/src/main/java/com/soa/gateway/ApiGatewayApplication.java
@@ -3,8 +3,9 @@ package com.soa.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
