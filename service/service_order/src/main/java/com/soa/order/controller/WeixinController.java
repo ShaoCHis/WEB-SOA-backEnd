5d3ebd7f516319eeca9f19bd1b9d48b33e5138b9
Bobo <572025842@qq.com>
2021-12-22 19:15:31 +0800

    update

--- service/service_order/src/main/java/com/soa/order/controller/WeixinController.java
+++ service/service_order/src/main/java/com/soa/order/controller/WeixinController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/order/weixin")
 @CrossOrigin
-@Api(value="微信支付",tags = "微信支付",description = "微信支付")
+@Api(value="微信支付",tags = "微信支付",description = "支付订单")
 public class WeixinController {
 
     @Autowired
